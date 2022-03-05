f867ef7e1c4201e717eef37054f523c1a824a73f
user <user@DESKTOP-QM1P8S9>
2022-03-05 20:17:28 +0530

    First Commit

---
+++ Basic/src/BasicJava/Do_WhileLoop.java
@@ -0,0 +1,21 @@
+/*
+The Java for loop is used to iterate a part of the program several times. 
+If the number of iteration is not fixed and you must have to execute the loop at least once,
+it is recommended to use a do-while loop.
+ */
+package BasicJava;
+
+public class Do_WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int i=1;    
+		    do
+		    {    
+		    System.out.println(i);    
+		    i++;    
+		    }
+		    while(i<=10); 
+	}
+
+}
---
+++ Basic/src/BasicJava/WhileLoop.java
@@ -0,0 +1,19 @@
+/*
+The Java for loop is used to iterate a part of the program several times. 
+If the number of iteration is not fixed, it is recommended to use for loop.
+ */
+package BasicJava;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;  
+	    while(i<=10)
+	    {  
+	       System.out.println(i);  
+	       i++;  
+	    }  
+	}
+
+}
