455e5fdcde2115d7fd03baef3dd045e1a0430dea
user <user@DESKTOP-QM1P8S9>
2022-03-13 16:26:15 +0530

    Fisrt Commit

--- Basic/src/day5/NonStaticMethod.java
+++ Basic/src/day5/NonStaticMethod.java
@@ -3,21 +3,31 @@ package day5;
 /* A non-static method can access a static variable  in Java */
 
 
-class StaticTest
-{
-	public static int i = 10;  //static variable
-	public void nonStatic()    // non static method
-	{ 
-		System.out.println("can access static variable inside non-static method : " + i); 
-	}
+class B {
+	 
+    // Static method
+    public int sum(int a, int b)
+    {
+        // Simply returning the sum
+        return a + b;
+    }
 }
 
 public class NonStaticMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+        int n = 12, m = 62;
+    
+        B g = new B();   // Creating object of above class
+ 
+        int s = g.sum(n, m);   // Calling above method to compute sum // Calling the non-static method 
+ 
+        System.out.print("sum is = " + s);
+		
 
-		System.out.println("Inside main method");
+		
 	}
 
 }
--- Basic/src/day5/StaticMethod.java
+++ Basic/src/day5/StaticMethod.java
@@ -1,20 +1,27 @@
 package day5;
 
 
-class A
-{
-    static int a=10,b=17;
-	public int sum(int a,int b)
-	{
-		return a+b;
-	}
+class A {
+	 
+    // Static method
+    public static int sum(int a, int b)
+    {
+        // Simply returning the sum
+        return a + b;
+    }
 }
 public class StaticMethod {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(A.a);
+        int n = 3, m = 6;
+ 
+        // Calling the static method of above class and storing sum in integer variable
+        int s = A.sum(n, m);
+ 
+        
+        System.out.print("sum is = " + s);
 	}
 
 }
