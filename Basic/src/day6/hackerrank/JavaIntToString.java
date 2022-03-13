1bf1cfcc076e0c591667f2bb34dd3c2012b9564a
user <user@DESKTOP-QM1P8S9>
2022-03-13 15:37:41 +0530

    First commit

---
+++ Basic/src/day6/hackerrank/JavaIntToString.java
@@ -0,0 +1,13 @@
+package day6.hackerrank;
+
+public class JavaIntToString {
+
+	public static void main(String[] args) {
+		int i=200;  
+		String s=Integer.toString(i);   // inbuild method 
+		System.out.println(i+100); 
+		System.out.println(s+100);
+
+	}
+
+}
