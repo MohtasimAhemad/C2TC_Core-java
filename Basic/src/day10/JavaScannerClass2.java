d3849a0cd65084ff3d74a2066bf5c5576faa8aff
user <user@DESKTOP-QM1P8S9>
2022-03-16 19:31:32 +0530

    First Commit

---
+++ Basic/src/day10/JavaScannerClass2.java
@@ -0,0 +1,29 @@
+package day10;
+
+import java.util.Scanner;
+
+public class JavaScannerClass2 {
+
+	public static void main(String[] args) {
+		String str = "Sum of 12 and 8 is equal 20 ";
+
+        // Instantiate Scanner with the given str
+        Scanner sc = new Scanner(str);
+
+        // Loop for scanning the double
+        // token input
+        while (sc.hasNext()) {
+
+            // if double then display it
+            if (sc.hasNextDouble()) {
+                System.out.println("Double Exists: " + sc.nextDouble());
+            }
+            sc.next();
+        }
+
+        // close the scanner
+        sc.close();
+
+	}
+
+}
