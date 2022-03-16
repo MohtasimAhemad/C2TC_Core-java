2273e2e5796c537a8db02bb96f5f71ed82e6a717
user <user@DESKTOP-QM1P8S9>
2022-03-16 19:47:55 +0530

    First Commit

--- Basic/src/day10/JavaScannerClass3.java
+++ Basic/src/day10/JavaScannerClass3.java
@@ -1,10 +1,22 @@
 package day10;
 
+import java.util.Scanner;
+
 public class JavaScannerClass3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+		System.out.print("Enter a string: ");  
+		String str= sc.nextLine();              //reads string  nextLine() method in java is present in the Scanner class and is used to get the input from the user. 
+		System.out.print("You have entered: "+str); 
+		
+		System.out.println("\n");
+		
+		System.out.print("Enter a string: ");  
+		String str1= sc.next();               //reads string before the space  
+		System.out.print("You have entered: "+str1);   
 	}
 
 }
--- Basic/src/day10/JavaScannerClass4.java
+++ Basic/src/day10/JavaScannerClass4.java
@@ -1,10 +1,26 @@
 package day10;
 
+import java.util.Scanner;
+
 public class JavaScannerClass4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		 System.out.print("Are you above 18 ? ");  
+         Scanner sc = new Scanner(System.in);
+         
+         boolean bn = sc.nextBoolean();  // reads a boolean value from the user
+         if (bn == true)
+         {  
+             System.out.println("You are over 18");  
+         } 
+         else if (bn == false)
+         {  
+             System.out.println("You are under 18");  
+         }  
+         // close the canner
+         sc.close();      
 	}
 
 }
