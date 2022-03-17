339bcc5321b31496dceb886282fa6b40a59d9982
user <user@DESKTOP-QM1P8S9>
2022-03-17 12:46:03 +0530

    First Commit

--- Basic/src/training/c2tc/day11/packagetwo/ClassTwo.java
+++ Basic/src/training/c2tc/day11/packagetwo/ClassTwo.java
@@ -1,9 +1,9 @@
-package training.c2tc.day11;
+package training.c2tc.day11.packagetwo;
 import training.c2tc.day11.packageone.ClassOne;  // i am import this user define package to access the class is present in this package
 public class ClassTwo {
 
 	 public void methodClassTwo(){
-	        System.out.println("Hello there i am ClassTwo");
+	        System.out.println(" ClassTwo  ");
 	    } 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
