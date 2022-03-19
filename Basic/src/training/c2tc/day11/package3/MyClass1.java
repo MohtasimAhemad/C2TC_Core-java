34c927e6d21d1d77cfca85573e202c69018f675d
user <user@DESKTOP-QM1P8S9>
2022-03-19 13:21:37 +0530

    First Commit

---
+++ Basic/src/training/c2tc/day11/package3/MyClass1.java
@@ -0,0 +1,20 @@
+package training.c2tc.day11.package3;  //user define package
+
+import java.util.ArrayList;           // BuiltIn package
+
+public class MyClass1 
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ArrayList<Integer> List = new ArrayList<>(3);
+		 
+	        List.add(3);
+	        List.add(2);
+	        List.add(1);
+	 
+	        System.out.println("The elements of list are: " + List);
+
+	}
+
+}
