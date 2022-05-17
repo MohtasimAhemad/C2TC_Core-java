d1c8a483bf2e421187936f7ddec035f8888a5adf
user <user@DESKTOP-QM1P8S9>
2022-05-17 15:50:47 +0530

    First Commit

---
+++ Basic/src/training/c2tc/day69/hibernateinheritance/joinedinheritance/HibernatejoinedinheritanceMain.java
@@ -0,0 +1,30 @@
+package training.c2tc.day69.hibernateinheritance.joinedinheritance;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernatejoinedinheritanceMain 
+{
+	public static void main(String[] args)
+	{
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction();
+		
+		Employee e = new Employee();
+		e.setEmpName("subash");
+		e.setEmpSalary(25000);
+		em.persist(e);
+		
+		Manager m = new Manager();
+		m.setEmpName("pooja");
+		m.setEmpSalary(60000);
+		em.persist(m);
+		
+		em.getTransaction().commit();
+		System.out.println("Rows Inserted.");
+		em.close();
+		factory.close();
+	}
+}
